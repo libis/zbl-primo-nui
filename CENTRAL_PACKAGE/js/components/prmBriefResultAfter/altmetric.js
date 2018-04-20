e0164b24a79f7f618eae99ac6e06b78fe642d52a
Mehmet Celik <mehmet@celik.be>
2018-04-20 14:08:30 +0200

    didn’t execute in some edge cases

--- CENTRAL_PACKAGE/js/components/prmBriefResultAfter/altmetric.js
+++ CENTRAL_PACKAGE/js/components/prmBriefResultAfter/altmetric.js
@@ -2,7 +2,7 @@ import altmetricHTML from './altmetric.html'
 
 /*
 //script needs to be loaded first. Can be put in the $onInit() function
-//or better in run method. 
+//or better in run method.
 app.run(($templateCache) => {
   Helper.loadScript('https://d1bxh8uas1mnw7.cloudfront.net/assets/embed.js?' + Date.now()).then(function () {
     console.log('altmerics.js loaded');
@@ -12,14 +12,18 @@ app.run(($templateCache) => {
 
 
 class AltMetricController {
-  constructor() {
+  constructor($element, $window, $scope) {
     var self = this;
     let item = self.parentCtrl.parentCtrl.item;
 
     self.doi = '';
     self.isbn = '';
+    self.id = self.guid();
+    self.recordid = '';
+
 
     if (item && item.pnx && item.pnx.addata) {
+      self.recordid = item.pnx.control.recordid[0];
       if (item.pnx.addata.doi) {
         self.doi = item.pnx.addata.doi[0];
       }
@@ -28,11 +32,41 @@ class AltMetricController {
         self.isbn = item.pnx.addata.isbn[0];
       }
     }
+
+    $scope.$watch(() => {
+      let altmetricLoaded = (typeof window._altmetric_embed_init) === 'function';
+      let isbnExists = document.querySelector(`#altmetric-isbn-${self.id}`) != null;
+      let doiExists = document.querySelector(`#altmetric-doi-${self.id}`) != null;
+      let runTrigger = ( altmetricLoaded && (isbnExists || doiExists) );
+
+      //console.log(self.id, altmetricLoaded, isbnExists, doiExists, runTrigger);
+      return runTrigger;
+    }, (n, o) => {
+      if (n == true) {
+        console.log("trigger altmetric for:", self.id);
+        $window._altmetric_embed_init(`#altmetric-isbn-${self.id}`);
+        $window._altmetric_embed_init(`#altmetric-doi-${self.id}`);
+      }
+    }, false);
+  }
+
+  guid() {
+    let s4 = function() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    };
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
   }
 }
 
+AltMetricController.$inject = ['$element', '$window', '$scope'];
+
 export let altmetricConfig = {
-  bindings: {parentCtrl: '<'},
+  bindings: {
+    parentCtrl: '<'
+  },
   controller: AltMetricController,
   template: altmetricHTML
 }
